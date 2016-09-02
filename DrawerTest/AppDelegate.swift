7a1896b7528b952af45fc291e4aa29de8b45c8d5
kbob <kbob@jogger-egg.com>
2016-09-02 16:55:49 -0700

    Initial Commit

---
+++ DrawerTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DrawerTest
+//
+//  Created by Bob Miller on 9/2/16.
+//  Copyright © 2016 kbobsoft.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
